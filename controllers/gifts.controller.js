d9b28b7687ce8b01888a4a602690f745eadda9d2
Joan Casals Salabert <39669781+Blackeagle5th@users.noreply.github.com>
2023-05-06 01:18:25 +0200

    Primera versió del codi dels regals

---
+++ controllers/gifts.controller.js
@@ -0,0 +1,29 @@
+const db = require('../daos/users.dao.js');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'mydatabase',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
+
+  //Retocar el codi
+  
+  app.post('/gifts', async (req, res) => {
+    try {
+      const { name, recipient, user_id } = req.body;
+      const connection = await pool.getConnection();
+      const [results] = await connection.execute(
+        'INSERT INTO gifts (name, recipient, user_id) VALUES (?, ?, ?)',
+        [name, recipient, user_id]
+      );
+      connection.release();
+      res.json({ id: results.insertId });
+    } catch (error) {
+      console.error('Error storing gift:', error);
+      res.status(500).json({ error: 'Error storing gift' });
+    }
+  });
