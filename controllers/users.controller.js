6306a3834b563b9b5806564e2eaf024f22b3e57a
eocana <eocana.joviat@gmail.com>
2023-05-25 18:34:09 +0200

    Update and hotfix

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,10 +1,22 @@
 const db = require("../daos/users.dao.js");
+const jwt = require('jsonwebtoken');
+
+
+
+function generateAccessToken(userId) {
+  return jwt.sign({ userId }, 'clave_secreta');
+}
+
 
 async function all(req, res) {
-
-    try {
+  try {
+    const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 10;
+    if (pageSize !== null || pageSize !== undefined || pageSize !== "" || pageSize !== 0 || pageSize !== NaN) {
+      const users = await db.getUsers(pageSize);
+    } else {
         const users = await db.all();
-        res.json(users);
+    }  
+    res.json(users);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
@@ -28,18 +40,76 @@ async function login(req, res) {
             res.status(401).json({ message: "Invalid credentials" });
             return;
         }
-        
-        res.status(200).json({});
+        console.log("Mi ID de usuario es: "+user.id);
+        const accessToken = generateAccessToken(user.id);
+        res.json({ accessToken });
 
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 }
 
+async function create(req, res) {
+    try {
+        const user = await db.create(req.body);
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+async function getLoggedInUser(req, res) {
+ 
+  try {
+    const userId = req.user.userId;
+    const user = await db.item(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+    res.json(user);
+  } catch (err) {
+    console.log("Estoy en getLoggedInUser catch: " + err);
+    res.status(500).json({ message: "err.message" });
+  }
+}
+
+async function editUser(req, res) {
+
+  try {
+    const userId = req.user.userId;
+
+    if (userId === "@me") {
+      console.log("Estoy editando mi perfil");
+    }
+
+    const user = await db.edit(userId);
+    if (!user) {
+      return res.status(404).json({ message: "err.message" });
+    }
+  } catch (err) {
+    console.log("Estoy en editUser catch: ");
+    res.status(500).json({ message: "err.message" });
+  }
+}
+
+async function searchUsers(req, res) {
+  try {
+    const email = req.query.s;
+    const users = await db.searchUsersByEmail(email);
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
 
 module.exports = {
   all,
-    item,
-    login,
+  item,
+  login,
+  create,
+  getLoggedInUser,
+  editUser,
+  searchUsers,
     
 };
--- daos/users.dao.js
+++ daos/users.dao.js
@@ -16,11 +16,12 @@ function all() {
 }
 
 function item(id) {
+  //console.log("Estoy en item: "+id);
   const stm = db.prepare("SELECT * FROM users WHERE id = ?");
   const rows = stm.get(id);
-  console.log(rows);
   return deletePassword(rows);
 }
+
 
 function login(email, password) {
   const stm = db.prepare("SELECT * FROM users WHERE email = ? AND password = ?");
@@ -28,9 +29,36 @@ function login(email, password) {
   return rows;
 }
   
+function create(user) {
+  const stm = db.prepare("INSERT INTO users (name, last_name, email, password, image) VALUES (?, ?, ?, ?, ?)");
+  const info = stm.run(user.name, user.last_name,user.email, user.password, user.image);
+  return item(info.lastInsertRowid);
+}
+
+function edit(userId) {
+  const stm = db.prepare("UPDATE users SET name = ?, last_name = ?, email = ?, password = ?, image = ? WHERE id = ?");
+  const info = stm.run(user.name, user.last_name, user.email, user.password, user.image, userId);
+  return item(info.lastInsertRowid);
+}
+
+function searchUsersByEmail(email) {
+  const stm = db.prepare('SELECT * FROM users WHERE email LIKE ?');
+  const users = stm.all(`%${email}%`);
+  return users;
+}
+
+function getUsers(pageSize) {
+  const stm = db.prepare('SELECT * FROM users LIMIT ?');
+  const users = stm.all(pageSize);
+  return users;
+}
 
 module.exports = {
   all,
   item,
-  login
+  login,
+  create,
+  edit,
+  searchUsersByEmail,
+  getUsers,
 };
